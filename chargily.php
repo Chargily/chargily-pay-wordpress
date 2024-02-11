5033219c4cdc1526c7904b86206eaed776fc727c
AA-Developer <ma7ad03@gmail.com>
2024-02-11 19:12:56 +0100

    Update chargily.php

--- chargily.php
+++ chargily.php
@@ -5,7 +5,7 @@
 *Description: Accept CIB and EDAHABIA cards on your WooCommerce store.
 *Author: Chargily
 Author URI: https://epay.chargily.com/
-*Version: 1.0.0
+*Version: 2.0.0
 *Text Domain: chargily-woocommerce-gateway
 */
 
