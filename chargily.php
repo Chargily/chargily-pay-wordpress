54a297ae43b386cad6de9e0e62c576757e563734
AA-Developer <ma7ad03@gmail.com>
2024-02-27 01:59:30 +0100

    Update chargily.php

--- chargily.php
+++ chargily.php
@@ -1,11 +1,11 @@
 <?php
 /*
-*Plugin Name: Chargily Pay™
+*Plugin Name: Chargily Pay
 *Plugin URI: https://chargily.com/
 *Description: Accept payments using CIB and EDAHABIA cards on your WooCommerce store.
-*Author: Chargily Pay™
+*Author: Chargily Pay
 Author URI: https://chargily.com/
-*Version: 2.0.9
+*Version: 1.0.0
 *Text Domain: chargilytextdomain
 *Domain Path: /languages
 */
@@ -37,15 +37,15 @@ add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'wc_chargily_g
 add_action('wp_enqueue_scripts', 'chargily_css_loader_front');
 function chargily_css_loader_front() {
     if ( is_checkout() ) {
-        wp_enqueue_style('chargily-style-front', plugins_url('/assets/css/css-front.css?v=209', __FILE__));
+        wp_enqueue_style('chargily-style-front', plugins_url('/assets/css/css-front.css?v=100', __FILE__));
 		 if (is_rtl()) {
-        	wp_enqueue_style('rtl-style',  plugins_url('/assets/css/css-front-rtl.css?v=209', __FILE__));
+        	wp_enqueue_style('rtl-style',  plugins_url('/assets/css/css-front-rtl.css?v=100', __FILE__));
     	}
     }
 }
 
 function chargily_js_loader_front() {
-    wp_enqueue_script( 'chargily-script-front', plugins_url('/assets/js/js-front.js?v=209', __FILE__), array('jquery'), null, true );
+    wp_enqueue_script( 'chargily-script-front', plugins_url('/assets/js/js-front.js?v=100', __FILE__), array('jquery'), null, true );
 }
 add_action( 'wp_enqueue_scripts', 'chargily_js_loader_front' );
 
@@ -102,3 +102,28 @@ function chargilyv2_add_rewrite_rule() {
 	add_rewrite_rule('^chargilyv2-webhook/?$', 'wp-content/plugins/chargily-pay/templates/method-v2/API-v2_webhook.php', 'top');
 }
 add_action('init', 'chargilyv2_add_rewrite_rule');
+
+register_activation_hook(__FILE__, 'update_chargily_pay_settings_data');
+add_action('upgrader_process_complete', 'update_chargily_pay_settings_data', 10, 2);
+function update_chargily_pay_settings_data() {
+    if ( is_admin() ) {
+        if (current_user_can('administrator') || current_user_can('shop_manager')) {
+            $test_mode = 'yes' === get_option('woocommerce_chargily_pay_settings')['test_mode'];
+            $live_api_key_present = !empty(get_option('woocommerce_chargily_pay_settings')['Chargily_Gateway_api_key_v2_live']);
+            $live_api_secret_present = !empty(get_option('woocommerce_chargily_pay_settings')['Chargily_Gateway_api_secret_v2_live']);
+            $test_api_key_present = !empty(get_option('woocommerce_chargily_pay_settings')['Chargily_Gateway_api_key_v2_test']);
+            $test_api_secret_present = !empty(get_option('woocommerce_chargily_pay_settings')['Chargily_Gateway_api_secret_v2_test']);
+        
+            $data = array(
+                'testMode' => $test_mode,
+                'liveApiKeyPresent' => $live_api_key_present,
+                'liveApiSecretPresent' => $live_api_secret_present,
+                'testApiKeyPresent' => $test_api_key_present,
+                'testApiSecretPresent' => $test_api_secret_present,
+            );
+        
+            $file_path = plugin_dir_path(__FILE__) . '/templates/method-v2/chargily_data.json';
+            file_put_contents($file_path, json_encode($data));
+        }
+    }
+}
