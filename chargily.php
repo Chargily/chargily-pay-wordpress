067d8815404ee972951c43137836104fcde86d4a
AA-Developer <ma7ad03@gmail.com>
2024-04-01 14:59:23 +0100

    Update chargily.php

--- chargily.php
+++ chargily.php
@@ -5,7 +5,7 @@
 *Description: The easiest and free way to integrate e-payment API through EDAHABIA of Algerie Poste and CIB of SATIM into your Wordpress/WooCommerce platform.
 *Author: Chargily
 Author URI: https://chargily.com
-*Version: 2.1.6
+*Version: 2.1.7
 *Text Domain: chargilytextdomain
 *Domain Path: /languages
 */
