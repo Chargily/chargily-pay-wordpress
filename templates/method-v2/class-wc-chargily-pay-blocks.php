b256b92e13268334a043413ce3ba4d209936e603
AA-Developer <ma7ad03@gmail.com>
2024-10-18 16:40:13 +0100

    Update class-wc-chargily-pay-blocks.php

--- templates/method-v2/class-wc-chargily-pay-blocks.php
+++ templates/method-v2/class-wc-chargily-pay-blocks.php
@@ -3,9 +3,11 @@ use Automattic\WooCommerce\Blocks\Payments\Integrations\AbstractPaymentMethodTyp
 
 final class WC_Chargily_Pay_Blocks extends AbstractPaymentMethodType {
     protected $name = 'chargily_pay';
+    protected $settings;
 
     public function initialize() {
         $this->settings = get_option('woocommerce_chargily_pay_settings', []);
+        $this->settings = array_map('sanitize_text_field', $this->settings);
     }
 
     public function is_active() {
@@ -14,20 +16,30 @@ final class WC_Chargily_Pay_Blocks extends AbstractPaymentMethodType {
 
     public function get_payment_method_script_handles() {
         wp_register_script(
-            'chargily-pay-blocks-integration',
-            plugins_url('../../assets/js/checkout.js', __FILE__),
-            ['wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-i18n'],
-            filemtime(plugin_dir_path(__FILE__) . '../../assets/js/checkout.js'),
-            true
-        );
-
+			'chargily-pay-blocks-integration',
+			plugins_url('../../assets/js/checkout.js', __FILE__),
+			['wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-i18n'],
+			filemtime(plugin_dir_path(__FILE__) . '../../assets/js/checkout.js'),
+			true
+		);
+		wp_localize_script(
+			'chargily-pay-blocks-integration',
+			'chargilySettings',
+			[
+				'title'       => isset($this->settings['title']) ? esc_html($this->settings['title']) : '',
+				'description' => isset($this->settings['description']) ? esc_html($this->settings['description']) : '',
+				'show_payment_methods' => isset($this->settings['show_payment_methods']) ? esc_html($this->settings['show_payment_methods']) : '',
+			]
+		);
         return ['chargily-pay-blocks-integration'];
     }
 
-    public function get_payment_method_data() {
-        return [
-            'title'       => $this->settings['title'],
-            'description' => $this->settings['description'],
-        ];
-    }
+	public function get_payment_method_data() {
+		$title = isset($this->settings['title']) ? esc_html($this->settings['title']) : __('Chargily Pay™', 'chargilytextdomain');
+		$description = isset($this->settings['description']) ? esc_html($this->settings['description']) : '';
+		return [
+			'title'       => $title,
+			'description' => $description,
+		];
+	}
 }
